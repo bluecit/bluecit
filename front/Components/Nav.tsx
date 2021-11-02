b80d1ea3d16318c385a179d6209e7d86f4aed40f
Amin <theaminux@gmail.com>
2021-11-02 23:19:37 +0300

    initialized :tea:

--- front/Components/Nav.tsx
+++ front/Components/Nav.tsx
@@ -1,13 +1,29 @@
 import * as React from "react";
 import styled from "styled-components";
 // Added staff
+
+const NavStyles = styled.div`
+  ul {
+    display: flex;
+    padding: 1rem;
+    li {
+      list-style: none;
+      padding: 1rem;
+    }
+  }
+`;
+
 const Nav = () => {
   return (
-    <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Services</li>
-    </ul>
+    <NavStyles>
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Products</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+    </NavStyles>
   );
 };
 
