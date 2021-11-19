140d06db1e130e7311f8f7a4760b899b95c4a8c3
Amin <theaminux@gmail.com>
2021-11-19 20:16:14 +0300

    removed unnecesary line of code

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,15 +1,8 @@
 import type { AppProps } from "next/app";
 import * as React from "react";
-// import useDarkMode from "use-dark-mode";
-import { ThemeProvider } from "styled-components";
-// import { DarkTheme as ToggleThemeIcon } from "@styled-icons/fluentui-system-filled";
 import Page from "../Components/Page";
-import { ToggleThemeStateProvider, useToggle } from "../utils/globalState";
-import {
-  GlobalStyles,
-  lightTheme,
-  darkTheme,
-} from "../Components/styles/ThemeConfig";
+import { ToggleThemeStateProvider } from "../utils/globalState";
+import { GlobalStyles } from "../Components/styles/ThemeConfig";
 
 const toggleIconStyle = {
   position: "absolute",
@@ -18,29 +11,14 @@ const toggleIconStyle = {
   zIndex: 100,
 };
 function MyApp({ Component, pageProps }: AppProps) {
-  // const { theme } = useToggle();
-  // const darkMode = useDarkMode(true);
-
-  // const theme = darkMode.value ? darkTheme : lightTheme;
-  // const [theme, setTheme] = React.useState("light");
   const [isMounted, setIsMounted] = React.useState(false);
-  // const toggleTheme = () => {
-  //   console.log("You clicked me");
-  //   theme == "light" ? setTheme("dark") : setTheme("light");
-  // };
   React.useEffect(() => {
     setIsMounted(true);
   }, []);
-  // theme={theme == "light" ? lightTheme : darkTheme}
+
   return (
     <ToggleThemeStateProvider>
       <GlobalStyles />
-      {/* <ToggleThemeIcon
-        onClick={toggleTheme}
-        size='50'
-        style={{ position: "absolute", top: 20, right: 20, zIndex: 100 }}
-      /> */}
-
       <Page>{isMounted && <Component {...pageProps} />}</Page>
     </ToggleThemeStateProvider>
   );
