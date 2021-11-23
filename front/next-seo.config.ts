8fd3cc2ff45b01730f77a8a331265d5cdeb47b6e
Amin <theaminux@gmail.com>
2021-11-23 03:58:58 +0300

    Added NextSEO

---
+++ front/next-seo.config.ts
@@ -0,0 +1,25 @@
+const title = "BlueCIT | Your Cybersecurity & Software Development Partner";
+const description = "More Description";
+
+const SEO = {
+  title,
+  description,
+  canonical: "https://bluecit.io",
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://bluecit.io",
+    title,
+    description,
+    images: [
+      {
+        url: "https://fastfeedback.io/og.png",
+        alt: title,
+        width: 1280,
+        height: 720,
+      },
+    ],
+  },
+};
+
+export default SEO;
