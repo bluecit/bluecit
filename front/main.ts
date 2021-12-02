73a8fd702488dc4650d3bc36702494ea2ee1abef
Amin <theaminux@gmail.com>
2021-12-02 21:30:21 +0300

    fix build next

---
+++ front/main.ts
@@ -0,0 +1,3 @@
+export const addTwoNumbers = (number1: any, number2: any) => {
+  // Add two numbers and return the result
+};
--- front/next.config.js
+++ front/next.config.js
@@ -1,11 +1,27 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 module.exports = {
-  reactStrictMode: true,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
+  future: {
+    webpack5: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      // issuer: {
+      //   test: /\.(js|ts)x?$/,
+      // },
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: {
+                removeViewBox: false,
+              },
+            },
+          },
+        },
+      ],
+    });
+    return config;
   },
 };
