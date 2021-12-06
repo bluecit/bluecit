95376cf81bfd54e35c346f8d7fb4ba6f95ba4d9d
Amin <theaminux@gmail.com>
2021-12-06 06:27:30 +0300

    removed Wavery

--- front/Components/Showcase.tsx
+++ front/Components/Showcase.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import styled from "styled-components";
-import WaverySVG from "@/components/Wavery";
+
 const SVGStyles = styled.svg`
   width: 100%;
   background: cover;
