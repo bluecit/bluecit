acab6ef10f23f632ca1a0f0c995e3a1190a176f1
Amin <theaminux@gmail.com>
2021-12-06 06:35:38 +0300

    nexjs config upgraded

--- front/next.config.js
+++ front/next.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
